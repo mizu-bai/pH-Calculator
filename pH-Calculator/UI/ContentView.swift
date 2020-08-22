4512b576217ca4003d437b12e79137c7091c54af
mizu-bai <shiragawa4519@outlook.com>
2020-08-22 15:32:53 +0800

    Gugugu

--- pH-Calculator/UI/ContentView.swift
+++ pH-Calculator/UI/ContentView.swift
@@ -11,8 +11,32 @@ struct ContentView: View {
     
     var body: some View {
         
-        Text("Hello, world!").padding()
-        
+        VStack {
+            
+            NekoImage()
+            
+            VStack(alignment: .leading, spacing: 10) {
+                
+                Text("pH Calculator")
+                    .font(.title)
+                
+                HStack {
+                    
+                    Text("by mizu-bai")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                    
+                Spacer()
+                    
+                }
+                
+                Text("差不多就差 UI 了，有缘再写，咕咕咕。")
+                    
+                
+            }
+            .padding()
+            
+        }
     }
 }
 
